e11d9f3ec550800c4b3a798220c6c601336c39f0
digaomatias <digaomatias@gmail.com>
2012-12-13 00:10:51 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeladaManager::Application.config.secret_token = '61bb92284ac3d59d71a6cd210e37fdefce5308c0fcf2da2dbd9e9c8d5cce07b059807510be3295f7ba06c90f30a74997d3a25a8d2f030476b4523b92a267ec8b'
