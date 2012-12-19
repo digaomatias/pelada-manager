75755a1a3dc313e66cac59d72893119587e031ff
digaomatias <digaomatias@gmail.com>
2012-12-19 01:12:25 -0200

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Pelada Manager'" do
+      visit '/static_pages/home'
+      page.should have_content('Pelada Manager')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+                                :text => "Pelada Manager | Home");
+    end
+
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+                                :text => "Pelada Manager | Help");
+    end
+  end
+
+  describe "About Page" do
+    
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+   it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+                                :text => "Pelada Manager | About");
+    end
+
+  end
+end
