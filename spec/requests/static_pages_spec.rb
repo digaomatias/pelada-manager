65eb40afac40974944a605d609f2f7a9e6341d9d
digaomatias <digaomatias@gmail.com>
2012-12-20 17:07:08 -0200

    Used let on base title to cut out some duplication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Pelada Manager" }
+
   describe "Home page" do
 
-    it "should have the content 'Pelada Manager'" do
+    it "should have the content 'Home'" do
       visit '/static_pages/home'
-      page.should have_content('Pelada Manager')
+      page.should have_content('Home')
     end
 
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-                                :text => "Pelada Manager | Home");
+                                :text => "#{base_title} | Home");
     end
 
   end
@@ -27,7 +29,7 @@ describe "Static pages" do
     it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-                                :text => "Pelada Manager | Help");
+                                :text => "#{base_title} | Help");
     end
   end
 
@@ -41,7 +43,7 @@ describe "Static pages" do
    it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title', 
-                                :text => "Pelada Manager | About");
+                                :text => "#{base_title} | About");
     end
 
   end
