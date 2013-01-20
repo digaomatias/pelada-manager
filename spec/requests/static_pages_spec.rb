22739e6a04637bcc376a367f0a43dbd3f00acfcc
digaomatias <digaomatias@gmail.com>
2013-01-20 20:54:51 -0200

    Added the full_title test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "Static pages" do
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title("Sign up")    
+  end
+
   subject { page }
   let(:base_title) { "Pelada Manager" }
 
