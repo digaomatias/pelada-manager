ab37fe8264de067d516651da167b3bfc8d178acb
digaomatias <digaomatias@gmail.com>
2013-01-24 07:25:45 -0200

    Added email downcase test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,6 +64,16 @@ describe User do
 		end
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "DIGAOMATIAS@GMail.com" }
+		
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
 	describe "when email address is already taken case insensitive" do
 		before do
 			user_with_same_email = @user.dup
